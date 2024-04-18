8f633ec7b0ccff4a8e5ab5b16724e387d7084834
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-04-18 09:39:36 +0200

    progetto ultimato

--- Unicam.Progetto4.Application/Abstractions/Services/IPrenotazioneService.cs
+++ Unicam.Progetto4.Application/Abstractions/Services/IPrenotazioneService.cs
@@ -5,7 +5,7 @@ namespace Unicam.Progetto4.Application.Abstractions.Services
     public interface IPrenotazioneService
     {
 
-        List<Prenotazione> GetPrenotazioni();
+        List<Prenotazione> GetPrenotazioni() ;
         List<Prenotazione> GetPrenotazioni(int from, int num, string? name, out int totalNum);
         void AddPrenotazione(Prenotazione prenotazione);
     }
